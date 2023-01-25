3a5f720c6ee2867eb0f47b95c041b6597b339506
Lorentz <91217473+Lauro235@users.noreply.github.com>
2023-01-25 21:22:17 +0000

    working data retrieval

--- backend/db/models/test.ts
+++ backend/db/models/test.ts
@@ -1,14 +1,13 @@
 import awsConnection from "../index"
 
 const test = () => {
-  awsConnection.connect((err) => {
-    if(!err) {
-      console.log("db is connected");
-    }
-    else {
-      console.log(err);
-    }
-  })
+  awsConnection.connect(function(err) {
+    if (err) throw err;
+    awsConnection.query("SELECT * FROM events WHERE event_type = 'mood_observation' ORDER BY timestamp DESC LIMIT 5;", function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  });
 }
 
 export default test;
