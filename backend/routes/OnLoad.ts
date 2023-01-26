799482300441d5ce16b8e9ccd47ee9317c5bdc0a
Lorentz <91217473+Lauro235@users.noreply.github.com>
2023-01-26 17:21:25 +0000

    create super onload route

--- backend/routes/OnLoad.ts
+++ backend/routes/OnLoad.ts
@@ -1,11 +1,14 @@
 import * as express from "express";
 import dbRowCount from "../db/models/dbRowCount"
+import timestampsToString from "../db/models/timestampToString"
 
 const OnLoad = express.Router()
 
 OnLoad.get("/", async (req, res) => {
     req == null
     const count = await dbRowCount();
+    console.log(await timestampsToString());
+    
     console.log(`This is the count: ${count}`);
   
     res.json(count)
